73c4b7b487b33c43ca8f73a1242b644a1e7dab7c
Mathias <mathias.schmerling@gmail.com>
2019-10-10 23:29:35 +0200

    Testing: datasets.y and sampler.py (unfinished)

---
+++ tests/test_datasets.py
@@ -0,0 +1,137 @@
+import unittest
+
+import torch
+from torch.utils.data import random_split, SubsetRandomSampler, WeightedRandomSampler
+from torchvision import transforms
+
+from few_shot_learning.datasets import FashionProductImages,\
+    FashionProductImagesSmall
+
+from config import DATA_PATH
+
+TOP20 = ["Jeans", "Perfume and Body Mist", "Formal Shoes",
+         "Socks", "Backpacks", "Belts", "Briefs",
+         "Sandals", "Flip Flops", "Wallets", "Sunglasses",
+         "Heels", "Handbags", "Tops", "Kurtas",
+         "Sports Shoes", "Watches", "Casual Shoes", "Shirts",
+         "Tshirts"]
+
+CLS = (FashionProductImagesSmall, FashionProductImages)
+
+SIZE = 44441
+
+
+class TestFashionProductImages(unittest.TestCase):
+    def test_default_config(self):
+        for cls in CLS:
+            if DATA_PATH is None:
+                self.assertRaises(Exception, cls, DATA_PATH)
+            else:
+                dataset = cls(DATA_PATH)
+
+    def test_split_arg(self):
+        wrong_arg = "split"
+        for cls in CLS:
+            self.assertRaises(AssertionError, cls, DATA_PATH, split=wrong_arg)
+
+    def test_top_bottom_classes(self):
+        for cls in CLS:
+            for split in ['test', 'train']:
+                # create top and bottom dataset
+                top = cls(DATA_PATH, split=split, classes='top')
+                bottom = cls(DATA_PATH, split=split, classes='bottom')
+
+                self.assertEqual(top.n_classes, 20,
+                                 "Attribute n_classes should be 20 for "
+                                 + "classes='top'")
+
+                self.assertTrue(top.samples[top.target_type].isin(TOP20).all(),
+                                "There are samples not in the top20 classes "
+                                "for classes='top'")
+                # self.assertTrue(dataset_top.targets.isin(TOP20).all())
+
+                self.assertFalse(
+                    bottom.samples[bottom.target_type].isin(TOP20).any(),
+                    "There are samples in the top20 classes for "
+                    "classes='bottom'")
+
+    def test_train_test_set(self):
+        for cls in CLS:
+            for classes in ['top', 'bottom']:
+                train = cls(DATA_PATH, split="train", classes=classes)
+                test = cls(DATA_PATH, split="test", classes=classes)
+
+                self.assertTrue((train.samples["year"] % 2 == 0).all(),
+                                "Training set contains samples from odd years")
+                self.assertFalse((test.samples["year"] % 2 == 0).any(),
+                                 "Test set contains samples from even years")
+
+    def test_size(self):
+        for cls in CLS[:1]:
+            total_size = 0
+            for split in ["train", "test"]:
+                for classes in ["top", "bottom"]:
+                    dataset = cls(DATA_PATH, split=split, classes=classes)
+                    total_size += len(dataset)
+            self.assertTrue(total_size == SIZE,
+                            "The size of all splits/class configs does not sum"
+                            "to {} for {}".format(SIZE, cls))
+
+            total_size = 0
+            for split in ["train", "test"]:
+                dataset = cls(DATA_PATH, split=split, classes=None)
+                total_size += len(dataset)
+            self.assertTrue(total_size == SIZE,
+                            "The size of all splits for all classes does not"
+                            "sum to {} for {}".format(SIZE, cls))
+
+    def _test_indices(self):
+        for cls in CLS:
+            for split in ["train", "test"]:
+                dataset = cls(DATA_PATH, split=split, classes=None)
+                for i in range(len(dataset)):
+                    _, _ = dataset[i]
+
+    def _test_image_format_and_transforms(self):
+        for cls in CLS:
+            transform1 = transforms.Compose([
+                transforms.Resize((80, 60)),
+            ])
+            dataset = cls(DATA_PATH, classes=None, transform=transform1)
+
+            all_correctly_sized = True
+            all_rgb = True
+            for i in range(len(dataset)):
+                X, _ = dataset[i]
+                all_correctly_sized &= X.size == (60, 80)
+                all_rgb &= X.mode == 'RGB'
+            self.assertTrue(all_correctly_sized)
+            self.assertTrue(all_rgb)
+
+            transform2 = transforms.Compose([
+                transforms.ToTensor(),
+            ])
+            dataset = cls(DATA_PATH, classes=None, transform=transform2)
+
+            all_torch_tensors = True
+            all_chw_format = True
+            for i in range(len(dataset)):
+                X, _ = dataset[i]
+                all_torch_tensors &= isinstance(X, torch.Tensor)
+                all_chw_format &= len(X.size) == 3
+            self.assertTrue(all_torch_tensors)
+            self.assertTrue(all_chw_format)
+
+
+class TestDataloaderOnFashionProductImages(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dataset = FashionProductImages(DATA_PATH, split='train', classes=None)
+
+    def test_random_split(self):
+        train_size = int(len(self.dataset) * 0.8)
+
+        trainset, valset = random_split(
+            self.dataset,[train_size, len(self.dataset) - train_size])
+
+        self.assertTrue(len(trainset) + len(valset) == len(self.dataset))
---
+++ tests/test_sampler.py
@@ -0,0 +1,136 @@
+import unittest
+
+import torch
+import pandas
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+
+from few_shot_learning.sampler import get_train_and_val_sampler
+
+
+class DummyUnbalancedDataset(Dataset):
+    def __init__(self, n_classes=100, n_features=1, max_samples_per_class=100,
+                 unstrafifiable=False):
+        """Dummy dataset for debugging/testing purposes
+
+        A sample from the DummyDataset has (n_features + 1) features. The first feature is the index of the sample
+        in the data and the remaining features are the class index.
+
+        # Arguments
+            samples_per_class: Number of samples per class in the dataset
+            n_classes: Number of distinct classes in the dataset
+            n_features: Number of extra features each sample should have.
+        """
+        self.n_classes = n_classes
+        self.n_features = n_features
+        self.max_samples_per_class = max_samples_per_class
+        self.samples_per_class = np.random.randint(0,
+                                                   self.max_samples_per_class,
+                                                   size=self.n_classes)
+        self.samples_per_class[0] = 0
+        if unstrafifiable:
+            self.samples_per_class[1] = 1
+
+        # Create a dataframe to be consistent with other Datasets
+        class_id = []
+        for i in range(self.n_classes):
+            class_id.extend([i] * self.samples_per_class[i])
+        self.samples = pandas.DataFrame({
+            'class_id': class_id
+        })
+        self.samples = self.samples.assign(id=self.samples.index.values)
+        self.target_indices = self.samples["class_id"]
+
+    def __len__(self):
+        return self.samples_per_class.sum()
+
+    def __getitem__(self, item):
+        X = np.random.normal(size=self.n_features)
+        y = self.samples["class_id"][item]
+        return X, y
+
+
+class TestTrainAndValSampler(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dataset = DummyUnbalancedDataset(max_samples_per_class=10000)
+        cls.unstrafifiable_dataset = \
+            DummyUnbalancedDataset(unstrafifiable=True)
+
+    def test_batch_sizes(self):
+        pass
+
+    def test_stratify_indices(self):
+        train_sampler, train_indices, val_sampler, val_indices = \
+            get_train_and_val_sampler(self.dataset,
+                                      train_size=0.6,
+                                      balanced_training=False,
+                                      stratify=True)
+
+        # test proportion of labels according to selected indices first
+        y_train_counts = np.bincount(
+            self.dataset.target_indices[train_indices],
+            minlength=self.dataset.n_classes)
+        y_val_counts = np.bincount(
+            self.dataset.target_indices[val_indices],
+            minlength=self.dataset.n_classes)
+
+        y_train_proportions = y_train_counts / y_train_counts.sum()
+        y_val_proportions = y_val_counts / y_val_counts.sum()
+
+        all_close = np.allclose(y_train_proportions, y_val_proportions,
+                                rtol=1e-2, atol=1e-2)
+
+        self.assertTrue(all_close,
+                        "Label proportions in indices for training and "
+                        "validation set are not close according to "
+                        "np.allclose")
+
+    def test_stratify_sampling(self):
+        train_sampler, train_indices, val_sampler, val_indices = \
+            get_train_and_val_sampler(self.dataset,
+                                      train_size=0.6,
+                                      balanced_training=False,
+                                      stratify=True)
+
+        # test proportion of labels when sampling
+        train_loader = \
+            DataLoader(self.dataset, batch_size=128, sampler=train_sampler)
+        val_loader = \
+            DataLoader(self.dataset, batch_size=128, sampler=val_sampler)
+
+        y_train_counts = np.zeros(self.dataset.n_classes)
+        y_val_counts = np.zeros(self.dataset.n_classes)
+        for _, y in train_loader:
+            y_train_counts += np.bincount(y, minlength=self.dataset.n_classes)
+        for _, y in val_loader:
+            y_val_counts += np.bincount(y, minlength=self.dataset.n_classes)
+
+        y_train_proportions = y_train_counts / y_train_counts.sum()
+        y_val_proportions = y_val_counts / y_val_counts.sum()
+
+        all_close = np.allclose(y_train_proportions, y_val_proportions,
+                                rtol=1e-2, atol=1e-2)
+
+        self.assertTrue(all_close,
+                        "Label proportions during sampling for training and "
+                        "validation set are not close according to "
+                        "np.allclose")
+
+    def test_unstratifiable(self):
+        self.assertRaises(Exception, get_train_and_val_sampler,
+                          self.unstrafifiable_dataset, stratify=True)
+
+    def test_train_size(self):
+        pass
+
+    def test_balanced_sampling(self):
+        pass
+
+    def test_no_overlap(self):
+        pass
+
+    def test_all_classes_in_trainset(self):
+        pass
+
